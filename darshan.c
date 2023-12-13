8709799380f6e4e5f79e718d089f47a34e11eaa1
darshan606 <153719552+darshan606@users.noreply.github.com>
2023-12-13 15:31:57 +0530

    Update darshan.c in dev2

--- darshan.c
+++ darshan.c
@@ -1,5 +1,8 @@
-#
-4432re
-  yetws5tg
-darshan.cdarshan.cdarshan.cdarshan.cdarshan.cdarshan.c
-UUUER
+#include<stdio.h>
+int main()
+{ int a=2,b=4,c,d;
+c=a*b;
+d=a%b;
+print(c);
+print(d);
+ return 0;}
